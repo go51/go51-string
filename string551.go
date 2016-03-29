ca86124b868d7f083ff04724966304f9a5075094
pubappbiz <ef+repository20160108@pubapp.biz>
2016-03-29 13:13:34 +0900

    refs #29 カタカナからひらがなに変更するメソッドを実装する

--- string551.go
+++ string551.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"unicode"
 	"unsafe"
 )
 
@@ -240,4 +241,31 @@ func CanonicalEmail(email string) string {
 	}
 
 	return BytesToString(ret)
+}
+
+var kanaConv unicode.SpecialCase = unicode.SpecialCase{
+	// ひらがなをカタカナに変換
+	unicode.CaseRange{
+		0x3041, // Lo: ぁ
+		0x3093, // Hi: ん
+		[unicode.MaxCase]rune{
+			0x30a1 - 0x3041, // UpperCase でカタカナに変換
+			0,               // LowerCase では変換しない
+			0x30a1 - 0x3041, // TitleCase でカタカナに変換
+		},
+	},
+	// カタカナをひらがなに変換
+	unicode.CaseRange{
+		0x30a1, // Lo: ァ
+		0x30f3, // Hi: ン
+		[unicode.MaxCase]rune{
+			0,               // UpperCase では変換しない
+			0x3041 - 0x30a1, // LowerCase でひらがなに変換
+			0,               // TitleCase では変換しない
+		},
+	},
+}
+
+func Katakana2Hiragana(src string) string {
+	return strings.ToLowerSpecial(kanaConv, src)
 }
